86ef54eb9b679530acf238227210c202b096bc97
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-19 16:07:37 +0600

    learn Go variable declarations

--- main.go
+++ main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
- fmt.Println("Hello, World!")
+
+	//   single variable declaration
+
+	//   var name string
+	//   name = "John Doe"
+
+	// var name string = "John Doe"
+
+	// name := "John Doe"
+
+	// var name = "John Doe"
+
+	// fmt.Println("Hello, World!")
+	// fmt.Println(name)
+
+	// group variable declaration
+	var (
+		name     string = "John Doe"
+		age      int    = 30
+		email    string = "john.doe@example.com"
+		lastName string = "Doe"
+	)
+	fmt.Println(name, age, email, lastName)
+
+	//  multiple variable declaration
+	var x, y int = 10, 20
+	// x = 10
+	// y = 20
+
+	fmt.Println(x, y)
 }
