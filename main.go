3864c739b294d23a444645bcebe735d6fe22a0be
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-21 04:59:56 +0600

    IIFE

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ func main() {
 
 	// Ananymous Function
 
-	// coffeeOrder := func() {
-	// 	fmt.Println("Coffee order placed")
-	// }
-	// coffeeOrder()
-
-	func() {
+	coffeeOrder := func() {
 		fmt.Println("Coffee order placed")
+	}
+	coffeeOrder()
 
-	}()
+	// Immediately Invoked Function Expression (IIFE)
+	func(CoffeeType string) {
+		fmt.Printf("Coffee order placed  %s.........", CoffeeType)
+
+	}("Latte")
 }
