3f3cafe386ecb6fb92141d4bc28f47a3316dec4e
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-21 04:52:34 +0600

    // Immediately Invoked Function Expression (IIFE)

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ import "fmt"
 
 func main() {
 
-	coffeeOrder := func() {
+	// Ananymous Function
+
+	// coffeeOrder := func() {
+	// 	fmt.Println("Coffee order placed")
+	// }
+	// coffeeOrder()
+
+	func() {
 		fmt.Println("Coffee order placed")
-	}
-	coffeeOrder()
+
+	}()
 }
