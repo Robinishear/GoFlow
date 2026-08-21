2fb8167cb538106da4494850274e1dfaa4c22503
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-22 01:54:04 +0600

    while styled loop

--- for-loop/main.go
+++ for-loop/main.go
@@ -8,9 +8,23 @@ func makeCoffee(x int) {
 
 func main() {
 
-	for i := 0; i <= 10; i++ {
-		// fmt.Println(i)
+	// for i := 0; i <= 10; i++ {   // for initialization; condition; increment/decrement
+	// 	// fmt.Println(i)
+	// 	makeCoffee(i)
+	// }
+
+	// while styled loop
+
+	i := 1
+
+	for i <= 5 {
 		makeCoffee(i)
 	}
-
 }
+
+// i := 1, true, run the body, increment
+// i = 2, true, run the body, increment
+// i = 3, true, run the body, increment
+// i = 4, true, run the body, increment
+// i = 5, true, run the body, increment
+// i = 6, false, stop
