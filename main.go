57d84e6b2517eb3f06e65682df992f72a7a289e8
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-21 23:24:26 +0600

    grading system

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 // import "fmt"
 
@@ -8,14 +8,25 @@ func main() {
 
 	// Ananymous Function
 
-	coffeeOrder := func() {
-		fmt.Println("Coffee order placed")
-	}
-	coffeeOrder()
+	// coffeeOrder := func() {
+	// 	fmt.Println("Coffee order placed")
+	// }
+	// coffeeOrder()
 
 	// Immediately Invoked Function Expression (IIFE)
-	func(CoffeeType string) {
-		fmt.Printf("Coffee order placed  %s.........", CoffeeType)
+	// func(CoffeeType string) {
+	// 	fmt.Printf("Coffee order placed  %s.........", CoffeeType)
 
-	}("Latte")
+	// }("Latte")
+
+	//
+
+	// mackCoffee := func() {
+	// 	Coffee := "Black Coffee"
+	// 	price := "150"
+
+	// 	fmt.Printf("Mack Coffee Price %s TK %s", Coffee, price)
+	// }
+	// mackCoffee()
+
 }
