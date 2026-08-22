4c11a928d5cf69453e152e1bd61e49cc1ec6874c
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-23 03:06:57 +0600

    get array length

--- Array/main.go
+++ Array/main.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
+// array type is [size]type and array is value type array length and capacity is same
 func main() {
 
 	var numbers [5]int
-	// numbers[0] = 10
+	numbers[0] = 10
 	numbers[1] = 20
-	// numbers[2] = 30
-	// numbers[3] = 40
-	// numbers[4] = 50
+	numbers[2] = 30
+	numbers[3] = 40
+	numbers[4] = 50
 
 	fmt.Println(numbers)
+	fmt.Println("Length", len(numbers))
 
 }
