780115e6b1ad118f23a8712186c0c13aeee8f35e
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-23 03:46:05 +0600

    start from index 3 and end at index 4 (exclusive of 4)

--- Slice/main.go
+++ Slice/main.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-// partial array initialize using slice operator [:end]
 func main() {
-	numbers := [6]int{10, 20, 30, 40, 50}
+	var orders = [6]int{10, 20, 30, 40, 50}
 
-	fmt.Println(numbers)
+	// fmt.Println(orders[:3])
+
+	Slice := orders[3:4]
+	fmt.Println(Slice)
 
 }
