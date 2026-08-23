6fb10ccd558ccc451d5743f0957ba7d61383f3fc
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-23 15:24:38 +0600

    slice literal

--- Slice/main.go
+++ Slice/main.go
@@ -3,11 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	var orders = [6]int{10, 20, 30, 40, 50}
+	// var orders = [6]int{10, 20, 30, 40, 50, 60}
 
-	// fmt.Println(orders[:3])
+	// // fmt.Println(orders[:3])
 
-	Slice := orders[3:4]
-	fmt.Println(Slice)
+	// slice := orders[2:5] // start from index 3 and end at index 4(exclusive of 4)
+	// fmt.Println(slice)
+
+	// // slice[0] = 500
+	// // fmt.Println(slice)
+	// // fmt.Println(orders)
+
+	// fmt.Println("len:", len(slice))
+	// fmt.Println("cap:", cap(slice))
+
+	var slice = []int{
+		100,
+		200,
+		300,
+		400,
+		500,
+		600,
+		700,
+		800,
+		900,
+		1000,
+	}
+
+	fmt.Println("The slice:", slice)
+	fmt.Println("Length of the slice:", len(slice))
+	fmt.Println("Capacity of the slice:", cap(slice))
 
 }
