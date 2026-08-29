9a4c99086a64b144ae7a2a4fd643530729414bcb
mdrobinahmed57898@gmail.com <mdrobinahmed57898@gmail.com>
2026-08-29 09:43:39 +0600

    Pointers → Pass by reference

--- Slice/main.go
+++ Slice/main.go
@@ -5,18 +5,19 @@ import "fmt"
 func main() {
 	// var orders = [6]int{10, 20, 30, 40, 50, 60}
 
-	// // fmt.Println(orders[:3])
+	// fmt.Println(orders[:3])
 
 	// slice := orders[2:5] // start from index 3 and end at index 4(exclusive of 4)
 	// fmt.Println(slice)
 
-	// // slice[0] = 500
-	// // fmt.Println(slice)
-	// // fmt.Println(orders)
+	// slice[0] = 500
+	// fmt.Println(slice)
+	// fmt.Println(orders)
 
 	// fmt.Println("len:", len(slice))
 	// fmt.Println("cap:", cap(slice))
 
+	// slice literal
 	var slice = []int{
 		100,
 		200,
@@ -29,6 +30,18 @@ func main() {
 		900,
 		1000,
 	}
+	slice[0] = 50
+	slice[1] = 150
+	slice[2] = 250
+	slice[3] = 350
+	slice[4] = 450
+	slice[5] = 550
+	slice[6] = 650
+	slice[7] = 750
+	slice[8] = 850
+	slice[9] = 950
+
+	slice = append(slice, 22)
 
 	fmt.Println("The slice:", slice)
 	fmt.Println("Length of the slice:", len(slice))
